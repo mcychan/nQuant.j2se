db86ff7c2a25c041047b4a5202c3f28758b0d57f
Miller Cy Chan <miller.chan@gmail.com>
2021-09-23 15:36:22 +0800

    Add files via upload

--- src/nQuant/j2se/PnnLABQuantizer.java
+++ src/nQuant/j2se/PnnLABQuantizer.java
@@ -510,10 +510,10 @@ public class PnnLABQuantizer extends PnnQuantizer {
 	protected short[] dither(final Color[] cPixels, Color[] palette, int nMaxColors, int width, int height, boolean dither)
     {		
 		short[] qPixels;
-		if ((nMaxColors < 64 && nMaxColors > 2) || hasSemiTransparency)
+		if ((nMaxColors < 64 && nMaxColors > 32) || hasSemiTransparency)
 			qPixels = quantize_image(cPixels, palette, dither);
-		else if(nMaxColors == 2)
-			qPixels = GilbertCurve.dither(width, height, cPixels, palette, getDitherFn(), 1.5f);
+		else if(nMaxColors <= 32)
+			qPixels = GilbertCurve.dither(width, height, cPixels, palette, getDitherFn(), nMaxColors > 2 ? 1.8f : 1.5f);
 		else			
 			qPixels = GilbertCurve.dither(width, height, cPixels, palette, getDitherFn());			
 		
