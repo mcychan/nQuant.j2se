1c7e430cd2da8b9a60f35b846ff29fe5b3df6c2f
Miller Cy Chan <miller.chan@gmail.com>
2025-01-28 14:35:37 +0800

    Add files via upload

--- src/nQuant/j2se/GilbertCurve.java
+++ src/nQuant/j2se/GilbertCurve.java
@@ -116,8 +116,8 @@ public class GilbertCurve {
 				c2 = BlueNoise.diffuse(pixel, palette[qPixels[bidx]], beta * .5f / saliencies[bidx], strength, x, y);
 
 			if (palette.length > 8 && CIELABConvertor.Y_Diff(pixel, c2) > (beta * acceptedDiff)) {
-				float lambda = saliencies[bidx] < .5f ? beta * .5f * saliencies[bidx] : beta * .5f / saliencies[bidx];
-				c2 = BlueNoise.diffuse(new Color(r_pix, g_pix, b_pix, a_pix), palette[qPixels[bidx]], lambda, strength, x, y);
+				float kappa = saliencies[bidx] < .5f ? beta * .5f * saliencies[bidx] : beta * .5f / saliencies[bidx];
+				c2 = BlueNoise.diffuse(new Color(r_pix, g_pix, b_pix, a_pix), palette[qPixels[bidx]], kappa, strength, x, y);
 			}
 
 			int offset = ditherable.getColorIndex(c2);
