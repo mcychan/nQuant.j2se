4358835f99bba8a2f351509ba2e011dad3bc0198
Miller Cy Chan <miller.chan@gmail.com>
2026-02-10 13:47:12 +0800

    Add files via upload

--- src/nQuant/j2se/GilbertCurve.java
+++ src/nQuant/j2se/GilbertCurve.java
@@ -142,43 +142,31 @@ public class GilbertCurve {
 				else
 					c2 = BlueNoise.diffuse(pixel, qPixel, beta * .5f / saliencies[bidx], strength, x, y);
 			}
-			if (CIELABConvertor.U_Diff(pixel, c2) > (margin * acceptedDiff))
-				c2 = BlueNoise.diffuse(pixel, qPixel, beta * normalDistribution(saliencies[bidx], .25f) + beta, strength, x, y);
 		}
 
-		if (margin > 6 || (palette.length <= 32 && weight < .01 && weight > .007)) {
-			if (palette.length > 4 && CIELABConvertor.Y_Diff(pixel, c2) > (beta * acceptedDiff)) {
-				float kappa = saliencies[bidx] < .4f ? beta * .4f * saliencies[bidx] : beta * .4f / saliencies[bidx];
-				Color c1 = new Color(r_pix, g_pix, b_pix, a_pix);
-				if (palette.length > 32 && saliencies[bidx] < .9)
-					kappa = beta * normalDistribution(saliencies[bidx], 2f);
-				else {
-					if (weight >= .0015 && saliencies[bidx] < .6)
-						c1 = pixel;
-					if (saliencies[bidx] < .6)
-						kappa = beta * normalDistribution(saliencies[bidx], weight < .0008 ? 2.5f : 1.75f);
-					else if (palette.length >= 32 || CIELABConvertor.Y_Diff(c1, c2) > (beta * Math.PI * acceptedDiff)) {
-						double ub = 1 - palette.length / 320.0;
-						if (saliencies[bidx] > .15 && saliencies[bidx] < ub)
-							kappa = beta * (!sortedByYDiff && weight < .0025 ? .55f : .5f) / saliencies[bidx];
-						else
-							kappa = beta * normalDistribution(saliencies[bidx], weight < .0025 ? 1.82f : 2f);
-					}
+		if (palette.length > 4 && CIELABConvertor.Y_Diff(pixel, c2) > (beta * acceptedDiff)) {
+			float kappa = saliencies[bidx] < .4f ? beta * .4f * saliencies[bidx] : beta * .4f / saliencies[bidx];
+			Color c1 = new Color(r_pix, g_pix, b_pix, a_pix);
+			if (palette.length > 32 && saliencies[bidx] < .9)
+				kappa = beta * normalDistribution(saliencies[bidx], 2f);
+			else {
+				if (weight >= .0015 && saliencies[bidx] < .6)
+					c1 = pixel;
+				if (saliencies[bidx] < .6)
+					kappa = beta * normalDistribution(saliencies[bidx], weight < .0008 ? 2.5f : 1.75f);
+				else if (palette.length >= 32 || CIELABConvertor.Y_Diff(c1, c2) > (beta * Math.PI * acceptedDiff)) {
+					double ub = 1 - palette.length / 320.0;
+					if (saliencies[bidx] > .15 && saliencies[bidx] < ub)
+						kappa = beta * (!sortedByYDiff && weight < .0025 ? .55f : .5f) / saliencies[bidx];
+					else
+						kappa = beta * normalDistribution(saliencies[bidx], weight < .0025 ? 1.82f : 2f);
 				}
-
-				c2 = BlueNoise.diffuse(c1, qPixel, kappa, strength, x, y);
 			}
-		}
-		else if (palette.length > 4 && (CIELABConvertor.Y_Diff(pixel, c2) > (beta * acceptedDiff) || CIELABConvertor.U_Diff(pixel, c2) > acceptedDiff)) {
-			if(beta < .4f && (palette.length <= 32 || saliencies[bidx] < beta))
-				c2 = BlueNoise.diffuse(c2, qPixel, beta * .4f * saliencies[bidx], strength, x, y);
-			else
-				c2 = new Color(r_pix, g_pix, b_pix, a_pix);
+
+			c2 = BlueNoise.diffuse(c1, qPixel, kappa, strength, x, y);
 		}		
 		
 		if (DITHER_MAX < 16 && palette.length > 4 && saliencies[bidx] < .6f && CIELABConvertor.Y_Diff(pixel, c2) > margin - 1)
-			c2 = new Color(r_pix, g_pix, b_pix, a_pix);
-		if (beta > 1f && CIELABConvertor.Y_Diff(pixel, c2) > DITHER_MAX)
 			c2 = new Color(r_pix, g_pix, b_pix, a_pix);
 
 		return ditherable.nearestColorIndex(palette, c2, bidx);
